8b33ebaa049fcacff1e69f944dab9bf1c7b6984a
kouyang <kouyang@gmail.com>
2018-08-26 23:13:30 -0500

    Reverse linked list since its been forever

--- dataStructure/stackLinkedList.js
+++ dataStructure/stackLinkedList.js
@@ -1,5 +1,5 @@
-function Node(data) {
-    this.data = data;
+function Node(val) {
+    this.val = val;
     this.next = null;
 }
 
@@ -9,12 +9,12 @@ class SinglyList {
     }
 
     isEmpty() {
-        return !!this.head.data;
+        return !!this.head.val;
     }
 
-    push(data) {
+    push(val) {
         const oldFirst = this.head;
-        const newFirst = new Node(data);
+        const newFirst = new Node(val);
         newFirst.next = oldFirst;
         this.head = newFirst;
     }
@@ -31,12 +31,33 @@ class SinglyList {
 const newSinglyList = new SinglyList();
 
 newSinglyList.push(5);
-newSinglyList.push(6);
-newSinglyList.push(9);
+newSinglyList.push(4);
+newSinglyList.push(3);
+newSinglyList.push(2);
+newSinglyList.push(1);
 
 console.log(newSinglyList);
 
-newSinglyList.pop();
-newSinglyList.pop();
+const reverseList = head => {
+    if (head === null) return null;
 
-console.log(newSinglyList);
+    let prev = null;
+    let next = null;
+
+    while (head) {
+        //Keep a reference for current next node in un-reversed order
+        next = head.next;
+        //Now set current head's next value to be what got us here, for head its null, for previous, its the next
+        head.next = prev;
+        //Set current to be the previous, thus future nodes know the previous one is
+        prev = head;
+        //Set head to equal the pointer we set aside earlier
+        head = next;
+    }
+
+    return prev;
+
+};
+
+console.log(reverseList(newSinglyList.head));
+
